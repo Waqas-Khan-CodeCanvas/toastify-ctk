3449a74e7dbb90ae98dfe89bd19452e4f8173b23
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-05-20 15:10:47 +0500

    widget:toast_label added

---
+++ src/toastify_ctk/widgets/message_label.py
@@ -0,0 +1,22 @@
+import customtkinter as ctk
+
+
+class MessageLabel(ctk.CTkLabel):
+
+    def __init__(
+        self,
+        parent,
+        text,
+        text_color,
+        wraplength
+    ):
+
+        super().__init__(
+            parent,
+            text=text,
+            text_color=text_color,
+            font=("Segoe UI", 12),
+            justify="left",
+            anchor="w",
+            wraplength=wraplength
+        )
