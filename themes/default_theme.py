2a7503f49abaa94ba46c59dadf62f0fc80172547
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-05-21 22:16:27 +0500

    centerilized theme added

---
+++ themes/default_theme.py
@@ -0,0 +1,12 @@
+THEME = {
+    "surface": "#1F1F1F",
+    "border": "#2C2C2C",
+    "text": "#FFFFFF",
+    "close": "#BBBBBB",
+    "progress_bg": "#333333",
+
+    "info": "#3498DB",
+    "success": "#2ECC71",
+    "warning": "#F1C40F",
+    "error": "#E74C3C",
+}
