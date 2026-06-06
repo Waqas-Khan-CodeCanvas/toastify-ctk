e12b96b54179ffac48ea5c5ad7da3458e571985c
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-06-06 11:36:34 +0500

    toast card upgraded

--- src/toastify_ctk/components/toast_host_window.py
+++ src/toastify_ctk/components/toast_host_window.py
@@ -1,9 +1,31 @@
+
+import customtkinter as ctk
+
+
+class ToastHostWindow(ctk.CTkFrame):
+    
+    def __init__(self , master):
+        super().__init__(master , fg_color="transparent")
+        self.place(relx=0 , rely=0 , relwidth=1  , relheight=1)
+        
+    def toast_container(self,x_offset=10 , y_offset=10):
+        self.toast_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.toast_frame.place(relx=0 , rely=0 ,x=x_offset , y=y_offset, anchor="nw") #anchor : must be n, ne, e, se, s, sw, w, nw, or center
+        return self.toast_frame
+
+
+
+
+
 # import customtkinter as ctk
 
 # from src.toastify_ctk.components.toast_card import ToastCard
 
 
 # class ToastHostWindow(ctk.CTkToplevel):
+#     """
+#     Single toast window container.
+#     """
 
 #     def __init__(
 #         self,
@@ -17,26 +39,18 @@
 #         height: int,
 #         x: int,
 #         y: int,
-#         on_close=None
+#         on_close=None,
 #     ):
-
 #         super().__init__(master)
 
-#         self.width = width
-#         self.height = height
-
-#         self._external_close = on_close
+#         self._on_close = on_close
 
 #         self.overrideredirect(True)
 #         self.attributes("-topmost", True)
 
-#         self.geometry(
-#             f"{width}x{height}+{x}+{y}"
-#         )
+#         self.geometry(f"{width}x{height}+{x}+{y}")
 
-#         self.configure(
-#             fg_color="#000001"
-#         )
+#         self.configure(fg_color="#000000")
 
 #         self.card = ToastCard(
 #             self,
@@ -44,105 +58,15 @@
 #             icon=icon,
 #             color=color,
 #             duration=duration,
-#             on_close=self._handle_close
+#             on_close=self._handle_close,
 #         )
 
-#         self.card.pack(
-#             fill="both",
-#             expand=True
-#         )
+#         self.card.pack(fill="both", expand=True)
 
-#         self.after(
-#             10,
-#             self.card.start
-#         )
+#         self.after(10, self.card.start)
 
 #     def _handle_close(self):
+#         if callable(self._on_close):
+#             self._on_close(self)
 
-#         if callable(self._external_close):
-#             self._external_close(self)
-
-#         if self.winfo_exists():
-#             self.destroy()
-
-#     def destroy_toast(self):
-
-#         try:
-#             self.card.destroy_toast()
-#         except Exception:
-#             pass
-
-#         if self.winfo_exists():
-#             self.destroy()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import customtkinter as ctk
-
-from src.toastify_ctk.components.toast_card import ToastCard
-
-
-class ToastHostWindow(ctk.CTkToplevel):
-    """
-    Single toast window container.
-    """
-
-    def __init__(
-        self,
-        master,
-        *,
-        message: str,
-        icon: str,
-        color: str,
-        duration: int,
-        width: int,
-        height: int,
-        x: int,
-        y: int,
-        on_close=None,
-    ):
-        super().__init__(master)
-
-        self._on_close = on_close
-
-        self.overrideredirect(True)
-        self.attributes("-topmost", True)
-
-        self.geometry(f"{width}x{height}+{x}+{y}")
-
-        self.configure(fg_color="#000000")
-
-        self.card = ToastCard(
-            self,
-            message=message,
-            icon=icon,
-            color=color,
-            duration=duration,
-            on_close=self._handle_close,
-        )
-
-        self.card.pack(fill="both", expand=True)
-
-        self.after(10, self.card.start)
-
-    def _handle_close(self):
-        if callable(self._on_close):
-            self._on_close(self)
-
-        self.destroy()
+#         self.destroy()
